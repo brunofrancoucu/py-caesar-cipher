7c0c9bee101b49038158755a28fb6c92293ab722
brunofrancoucu <bruno.franco@correo.ucu.edu.uy>
2026-04-16 14:53:52 -0300

    refactor: constants into config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+"""abecedario en minuscula"""
+abc = "abcdefghijklmnopqrstuvwxyz"
+
+"""abecedario en minuscula acentuado"""
+abcAc = "ábcdéfghíjklmnópqrstúvwxyz"
+
+"""abecedario en mayusculas"""
+ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+"""abecedario en mayusculas acentuado"""
+ABCAc = ABC.upper()
+
+"""abecedario permutado de a pares (ab <=> ba, cd <==> dc)"""
+bac = "badcfehgjilknmporqtsvuxwzy"
